082da80c9b7193750c5ddbbf31ad918246dc2d93
Falsher <ivan.zzaharov@gmail.com>
2021-05-17 21:28:44 +0300

    git added d

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 class CountdownTimer {
   constructor(conf) {
     const target = document.querySelector(conf.selector);
-    const dateString = target.querySelectorAll(".value");
+    const dateFields = target.querySelectorAll(".value");
     setInterval(() => {
       const currentTime = new Date();
+    
       const dateDiffSec = conf.targetDate - currentTime;
-      dateString[0].innerHTML = this.days(dateDiffSec);
-      dateString[1].innerHTML = this.hours(dateDiffSec);
-      dateString[2].innerHTML = this.mins(dateDiffSec);
-      dateString[3].innerHTML = this.secs(dateDiffSec);
+      dateFields[0].innerHTML = this.days(dateDiffSec);
+      dateFields[1].innerHTML = this.hours(dateDiffSec);
+      dateFields[2].innerHTML = this.mins(dateDiffSec);
+      dateFields[3].innerHTML = this.secs(dateDiffSec);
     }, 1000);
   }
 
@@ -18,7 +19,6 @@ class CountdownTimer {
   mins = (time) => Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
   secs = (time) => Math.floor((time % (1000 * 60)) / 1000);
 }
-
 new CountdownTimer({
   selector: "#timer-1",
   targetDate: new Date("Jul 17, 2019"),
