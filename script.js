62f680dc05196f687f626ca0a7608cfd9029849e
Falsher <ivan.zzaharov@gmail.com>
2021-05-18 23:42:04 +0300

    git added end

--- script.js
+++ script.js
@@ -1,27 +1,35 @@
 class CountdownTimer {
-  constructor(conf) {
-    const target = document.querySelector(conf.selector);
-    const dateFields = target.querySelectorAll(".value");
-    setInterval(() => {
-      const currentTime = new Date();
-
-      const dateDiffSec = conf.targetDate - currentTime;
-
-      dateFields[0].innerText = this.days(dateDiffSec);
-      dateFields[1].innerText = this.hours(dateDiffSec);
-      dateFields[2].innerText = this.mins(dateDiffSec);
-      dateFields[3].innerText = this.secs(dateDiffSec);
-    }, 0);
+  timeNum = { days: 0, hours: 0, mins: 0, secs: 0 };
+  constructor({selector, targetDate}) {
+    const target = document.querySelector(selector);
+    this.dateFields = target.querySelectorAll(".value");
+    this.targetDate = targetDate
   }
 
-  days = (time) => Math.floor(time / (1000 * 60 * 60 * 24));
-  hours = (time) =>
-    Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  mins = (time) => Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-  secs = (time) => Math.floor((time % (1000 * 60)) / 1000);
+  calculateTimeNum(time) {
+    this.timeNum.days = Math.floor(time / (1000 * 60 * 60 * 24));
+    this.timeNum.hours = Math.floor(
+      (time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
+    );
+    this.timeNum.mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+    this.timeNum.secs = Math.floor((time % (1000 * 60)) / 1000);
+  }
+  updateDate() {
+    this.dateFields[0].innerText =this.timeNum.days
+    this.dateFields[1].innerText = this.timeNum.hours
+    this.dateFields[2].innerText = this.timeNum.mins
+    this.dateFields[3].innerText = this.timeNum.secs
+  }
+  startTimer() {
+    setInterval(() => {
+      this.calculateTimeNum(this.targetDate- Date.now());
+      this.updateDate();
+    }, 1000);
+  }
 }
 
-new CountdownTimer({
+const birthdayTimer = new CountdownTimer({
   selector: "#timer-1",
-  targetDate: new Date("Jul 17, 2021"),
+  targetDate: new Date("Jun 03, 2021"),
 });
+birthdayTimer.startTimer();
